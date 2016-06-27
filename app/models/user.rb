9026c5aa3a5673be61f73c560df80f17a6567267
nuenvio <nuenvio@gmail.com>
2016-06-27 08:53:47 +0300

    request model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :requests
+  has_many :projects, through: :requests
 end
