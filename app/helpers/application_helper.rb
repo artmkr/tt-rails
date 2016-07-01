69af3aa1bd1b892d5b86ac3231c1b04cdc3302f7
nuenvio <nuenvio@gmail.com>
2016-07-01 10:10:56 +0300

    flash notices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,11 +13,20 @@ module ApplicationHelper
     
     Redcarpet::Markdown.new(renderer, options).render(text).html_safe
   end
+
   def nav_link(text, path)
     options = current_page?(path) ? { class: "active" } : {}
     content_tag(:li, options) do
       link_to text, path
     end
+  end
+
+  def bootstrap_class_for flash_type
+    { success: "alert-success",
+      error: "alert-error",
+      alert: "alert-warning",
+      notice: "alert-info"
+    } [flash_type.to_sym] || flash_type.to_s
   end
 end
 
