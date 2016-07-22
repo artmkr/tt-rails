32294faf6303bfd8ad1d1e54f2de7b99228fdbca
nuenvio <nuenvio@gmail.com>
2016-07-22 18:09:02 +0300

    fix mailgun adress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
     user_name:      ENV['SENDMAIL_USERNAME'],
     password:       ENV['SENDMAIL_PASSWORD'],
     domain:         ENV['MAIL_HOST'],
-    address:       'smtp.mailgun.or',
+    address:       'smtp.mailgun.org',
     port:          '587',
     authentication: :plain,
     enable_starttls_auto: true
