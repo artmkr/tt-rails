ed443459205d4c28cb66da13e21f0ae7b1acb726
nuenvio <nuenvio@gmail.com>
2016-07-22 17:37:24 +0300

    env host fixes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'nuenvio@tempel.tech'
+  config.mailer_sender = 'support@tempel.tech'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
